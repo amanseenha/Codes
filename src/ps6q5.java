7e1d86248cd5fcde7df5d9247d803e2b3ec0d22a
amanseenha <2105521@kiit.ac.in>
2022-07-24 18:33:34 +0530

    3rd

--- src/ps6q5.java
+++ src/ps6q5.java
@@ -1,10 +1,10 @@
 public class ps6q5 {
     public static void main(String[] args) {
-        int [] arr ={1,2,3,4,5,6};
-        int len= arr.length;
-        int n= Math.floorDiv(len,2);
-        int temp;
-        for (int i=0;i<n;i++){
+        int [] arr= {1,2,3,4,5,6,7};
+        int len = arr.length;
+        int temp=0;
+        int division=Math.floorDiv(len,2);
+        for (int i=0;i<division;i++) {
             temp=arr[i];
             arr[i]=arr[len-i-1];
             arr[len-i-1]=temp;
