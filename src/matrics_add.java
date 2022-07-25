1a3c2d2dd31a22e0f79e7d40ff999b028d5fa8ed
amanseenha <2105521@kiit.ac.in>
2022-07-25 06:20:46 +0530

    3rd

--- src/matrics_add.java
+++ src/matrics_add.java
@@ -1,7 +1,7 @@
 public class matrics_add {
     public static void main(String[] args) {
         int[][] mat1 = {{1, 2, 3},
-                        {1, 2, 3}};
+                        {4, 5, 6}};
 
         int[][] mat2 = {{2, 3, 4},
                         {5, 6, 7}};
