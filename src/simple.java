1ee64d14ff9073e0833176612a89d3c51629160f
amanseenha <2105521@kiit.ac.in>
2022-07-26 20:21:50 +0530

    Java

---
+++ src/simple.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class simple {
+    public static void main(String[] args) {
+        Scanner nam    = new Scanner(System.in);
+        Scanner rollno = new Scanner(System.in);
+        System.out.print("Enter your Name: ");
+        String name = nam.nextLine();
+        System.out.print("Enter your Roll No.: ");
+        int roll = rollno.nextInt();
+        System.out.println("Your name is: "+ name);
+        System.out.print("Your roll no. is: "+ roll);
+    }
+}
