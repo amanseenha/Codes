71839c9b30c0cfedab1c9b57771c084763c7d647
amanseenha <2105521@kiit.ac.in>
2022-12-31 14:54:59 +0530

    concat

--- src/concat.java
+++ src/concat.java
@@ -2,6 +2,6 @@ public class concat {
     public static void main(String[] args) {
         String str1 = "Aman";
         String str2 = "Sinha";
-        
+        System.out.print(str1+str2);
     }
 }
