d13132d8debf1330806b02e14a06c106e1f0fe48
amanseenha <2105521@kiit.ac.in>
2023-01-23 23:54:29 +0530

    added .gitignore

--- src/test.java
+++ src/test.java
@@ -1,6 +1,9 @@
+import java.util.Arrays;
+
 public class test {
 
-    public static void main(String[] args) {
-        System.out.println("Cloned by Java Repository");
-    }
+  public static void main(String[] args) {
+    int[] arr = { 1, 2, 3, 4, 5 };
+    System.out.println(Arrays.toString(arr));
+  }
 }
