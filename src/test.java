b794e78db078054d24a82d00185105223e2e01ea
amanseenha <2105521@kiit.ac.in>
2023-02-19 12:05:10 +0530

    modified .gitignore

--- src/test.java
+++ src/test.java
@@ -1,9 +1,21 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class test {
 
   public static void main(String[] args) {
-    int[] arr = { 1, 2, 3, 4, 5 };
-    System.out.println(Arrays.toString(arr));
+    int[][] arr = new int[3][2];
+    Scanner sc = new Scanner(System.in);
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < arr[i].length; j++) {
+        arr[i][j] = sc.nextInt();
+      }
+    }
+
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < arr[i].length; j++) {
+        System.out.println(arr[1][1]);
+      }
+    }
+    sc.close();
   }
 }
